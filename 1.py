4ad6168ce18360efba48e04696f091e0be606ec7
Иброхим Абдимуродов <118828452+IbrohimAbdimurodov@users.noreply.github.com>
2022-11-23 21:37:20 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+import time
+start_time= time.time()
+def fun():
+    a=3
+    b=4
+    c=a+b
+end_time= time.time()
+fun()
+vaqt1 = end_time - start_time
+print("Dasturga ketadigan vaqt: ", vaqt1)
